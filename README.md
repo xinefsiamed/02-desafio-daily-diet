dd1c1948f091ef27cea7738b73097854a73a16fa
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-05-10 17:33:03 -0300

    Creating routes to add, update and delete diets

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 #RF
 
 - [x] Deve ser possivel criar um usuario
-- [ ] Deve ser possível criar uma refeição (nome, descrição, data e hora, esta dentro ou não da dieta )
-- [ ] Deve ser possível editar uma refeição
-- [ ] Deve ser possível apagar uma refeição
-- [ ] Deve ser possível visualizar uma única refeição
+- [x] Deve ser possível criar uma refeição (nome, descrição, data e hora, esta dentro ou não da dieta )
+- [x] Deve ser possível editar uma refeição
+- [x] Deve ser possível apagar uma refeição
+- [x] Deve ser possível visualizar uma única refeição
 - [x] Deve ser possível listar todas as refeições de um usuário
 
 #RN
 
 - [x] Deve ser possivel identificar um usuário entre as requisiçoes
-- [ ] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou
+- [x] O usuário só pode visualizar, editar e apagar as refeições o qual ele criou
 - [ ] Deve ser possível recuperar as métricas de um usuário
--- src/routes/diets.ts
+++ src/routes/diets.ts
@@ -38,4 +38,100 @@ export async function DietsRoutes(app: FastifyInstance) {
 
     return { diets }
   })
+
+  app.get('/:id', { onRequest: [app.authenticate] }, async (request, reply) => {
+    const routeParam = zod.object({
+      id: zod.string().uuid().nonempty({ message: 'id cannot be empty.' }),
+    })
+
+    const { id } = routeParam.parse(request.params)
+
+    const diet = await knex('diets').where({ id }).first()
+
+    if (!diet) {
+      reply.status(404).send({ message: 'diet cannot be found' })
+      return
+    }
+
+    if (diet.user_id !== request.user.sub) {
+      reply.status(401)
+      return
+    }
+
+    return {
+      diet,
+    }
+  })
+
+  app.patch(
+    '/:id',
+    { onRequest: [app.authenticate] },
+    async (request, reply) => {
+      const dietBody = zod.object({
+        name: zod.string(),
+        description: zod.string(),
+        dateTime: zod.string().datetime(),
+        isOnDiet: zod.boolean(),
+      })
+
+      const routeParam = zod.object({
+        id: zod.string().uuid().nonempty({ message: 'id cannot be empty.' }),
+      })
+
+      const { name, description, dateTime, isOnDiet } = dietBody.parse(
+        request.body,
+      )
+
+      const { id } = routeParam.parse(request.params)
+
+      const diet = await knex('diets').where({ id }).first()
+
+      if (!diet) {
+        reply.status(404).send({ message: 'Diet not found!' })
+        return
+      }
+
+      if (diet.user_id !== request.user.sub) {
+        reply.status(401)
+        return
+      }
+
+      await knex('diets').where({ id }).update({
+        name,
+        date_time: dateTime,
+        description,
+        is_on_diet: isOnDiet,
+      })
+
+      reply.status(201)
+    },
+  )
+
+  app.delete(
+    '/:id',
+    { onRequest: [app.authenticate] },
+    async (request, reply) => {
+      const routeParam = zod.object({
+        id: zod.string().uuid().nonempty({ message: 'id cannot be empty.' }),
+      })
+
+      const { id } = routeParam.parse(request.params)
+
+      const diet = await knex('diets').where({ id }).first()
+
+      if (!diet) {
+        reply.status(404).send({ message: 'Diet not found.' })
+        return
+      }
+
+      if (diet.user_id !== request.user.sub) {
+        reply.status(401)
+        return
+      }
+
+      await knex('diets').where({ id }).delete()
+
+      reply.status(200)
+    },
+  )
 }
